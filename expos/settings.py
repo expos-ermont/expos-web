cb425f5f4df10b5d927b3b65258999f347586c5e
Florent Captier <florent@captier.org>
2015-04-17 00:37:08 +0200

    Change default sqlite db

--- expos/settings.py
+++ expos/settings.py
@@ -15,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/florent/Sites/expos_django/expos/db/expos.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': 'expos.sqlite',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
